bdac970fb683a1a72bc3098e634f765ca1914806
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-08-22 22:21:07 -0400

    refactor code to account for exact change in cash drawer

--- javascript-algorithms-and-data-structures-projects/cashRegister.js
+++ javascript-algorithms-and-data-structures-projects/cashRegister.js
@@ -1,19 +1,21 @@
 function checkCashRegister(price, cash, cid) {
   let changeDue = cash - price;
   let startingCash = 0.0;
-  let currentCash;
+  let currentCash, cashIndex, denomination;
+  let changeGiven = 0.0;
+  let exactChange = 0.0;
 
-  const cashEquiv = {
-    ONE: 0.01,
-    NICKEL: 0.05,
-    DIME: 0.1,
-    QUARTER: 0.25,
-    DOLLAR: 1.0,
-    FIVE: 5.0,
-    TEN: 10.0,
-    TWENTY: 20.0,
-    "ONE HUNDRED": 100.0
-  };
+  // const cashEquiv = {
+  //   ONE: 0.01,
+  //   NICKEL: 0.05,
+  //   DIME: 0.10,
+  //   QUARTER: 0.25,
+  //   DOLLAR: 1.00,
+  //   FIVE: 5.00,
+  //   TEN: 10.00,
+  //   TWENTY: 20.00,
+  //   "ONE HUNDRED": 100.00
+  // }
 
   function findCashIndex(arr, val) {
     for (let i = 0; i < arr.length; i++) {
@@ -23,8 +25,6 @@ function checkCashRegister(price, cash, cid) {
     }
   }
 
-  console.log(findCashIndex(cid, "NICKEL"));
-
   function countMoney(moneyArr, currentAmt) {
     for (let i = 0; i < moneyArr.length; i++) {
       currentAmt += moneyArr[i][1];
@@ -32,9 +32,42 @@ function checkCashRegister(price, cash, cid) {
     return currentAmt.toFixed(2);
   }
 
-  currentCash = countMoney(cid, startingCash);
+  // currentCash = countMoney(cid, startingCash);
 
-  if (changeDue > currentCash) {
+  switch (true) {
+    case changeDue > 100.0:
+      denomination = "ONE HUNDRED";
+      break;
+    case changeDue > 20.0:
+      denomination = "TWENTY";
+      break;
+    case changeDue > 10.0:
+      denomination = "TEN";
+      break;
+    case changeDue > 5.0:
+      denomination = "Five";
+      break;
+    case changeDue > 1.0:
+      denomination = "ONE";
+      break;
+    case changeDue > 0.25:
+      denomination = "QUARTER";
+      break;
+    case changeDue > 0.1:
+      denomination = "DIME";
+      break;
+    case changeDue > 0.05:
+      denomination = "NICKEL";
+      break;
+    default:
+      denomination = "PENNY";
+  }
+
+  cashIndex = findCashIndex(cid, denomination);
+  let exactChangeArr = cid.slice(0, cashIndex + 1);
+  exactChange = countMoney(exactChangeArr, exactChange);
+
+  if (changeDue > exactChange) {
     return { status: "INSUFFICIENT_FUNDS", change: [] };
   }
 }
