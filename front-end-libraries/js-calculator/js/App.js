1fb0cf3a8bdc9a4bbad415fe1751a7776f245c5a
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-09-10 17:29:20 -0400

    refactor Buttons component

--- front-end-libraries/js-calculator/js/App.js
+++ front-end-libraries/js-calculator/js/App.js
@@ -1,3 +1,63 @@
+class Buttons extends React.Component {
+  render() {
+    return (
+      <div id="buttons">
+        <button id="decimal" value="." onClick={this.props.numbers}>
+          .
+        </button>
+        <button id="zero" value="0" onClick={this.props.numbers}>
+          0
+        </button>
+        <button id="one" value="1" onClick={this.props.numbers}>
+          1
+        </button>
+        <button id="two" value="2" onClick={this.props.numbers}>
+          2
+        </button>
+        <button id="three" value="3" onClick={this.props.numbers}>
+          3
+        </button>
+        <button id="four" value="4" onClick={this.props.numbers}>
+          4
+        </button>
+        <button id="five" value="5" onClick={this.props.numbers}>
+          5
+        </button>
+        <button id="six" value="6" onClick={this.props.numbers}>
+          6
+        </button>
+        <button id="seven" value="7" onClick={this.props.numbers}>
+          7
+        </button>
+        <button id="eight" value="8" onClick={this.props.numbers}>
+          8
+        </button>
+        <button id="nine" value="9" onClick={this.props.numbers}>
+          9
+        </button>
+        <button id="clear" onClick={this.props.clear}>
+          AC
+        </button>
+        <button id="divide" value="/" onClick={this.props.functions}>
+          /
+        </button>
+        <button id="multiply" value="*" onClick={this.props.functions}>
+          *
+        </button>
+        <button id="add" value="+" onClick={this.props.functions}>
+          +
+        </button>
+        <button id="subtract" value="-" onClick={this.props.functions}>
+          -
+        </button>
+        <button id="equals" value="=">
+          =
+        </button>
+      </div>
+    );
+  }
+}
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
@@ -5,7 +65,8 @@ class Calculator extends React.Component {
       output: "",
       display: 0
     };
-    this.numberClick = this.numberClick.bind(this);
+    this.handleNumbers = this.handleNumbers.bind(this);
+    this.handleFunctions = this.handleFunctions.bind(this);
     this.clearDisplay = this.clearDisplay.bind(this);
   }
 
@@ -16,7 +77,14 @@ class Calculator extends React.Component {
     });
   }
 
-  numberClick(e) {
+  handleNumbers(e) {
+    this.setState({
+      output: (this.state.output += e.target.value),
+      display: e.target.value
+    });
+  }
+
+  handleFunctions(e) {
     this.setState({
       output: (this.state.output += e.target.value),
       display: e.target.value
@@ -31,56 +99,11 @@ class Calculator extends React.Component {
             <div id="calculator">
               <div id="output">{this.state.output}</div>
               <div id="display">{this.state.display}</div>
-              <div id="buttons">
-                <button id="decimal">.</button>
-                <button id="zero">0</button>
-                <button id="one" value="1" onClick={this.numberClick}>
-                  1
-                </button>
-                <button id="two" value="2" onClick={this.numberClick}>
-                  2
-                </button>
-                <button id="three" value="3" onClick={this.numberClick}>
-                  3
-                </button>
-                <button id="four" value="4" onClick={this.numberClick}>
-                  4
-                </button>
-                <button id="five" value="5" onClick={this.numberClick}>
-                  5
-                </button>
-                <button id="six" value="6" onClick={this.numberClick}>
-                  6
-                </button>
-                <button id="seven" value="7" onClick={this.numberClick}>
-                  7
-                </button>
-                <button id="eight" value="8" onClick={this.numberClick}>
-                  8
-                </button>
-                <button id="nine" value="9" onClick={this.numberClick}>
-                  9
-                </button>
-                <button id="clear" onClick={this.clearDisplay}>
-                  AC
-                </button>
-                <button id="divide" value="/">
-                  /
-                </button>
-                <button id="multiply" value="*">
-                  *
-                </button>
-                <button id="add" value="+">
-                  +
-                </button>
-                <button id="subtract" value="-">
-                  -
-                </button>
-                <button id="equals" value="=">
-                  {" "}
-                  ={" "}
-                </button>
-              </div>
+              <Buttons
+                numbers={this.handleNumbers}
+                functions={this.handleFunctions}
+                clear={this.clearDisplay}
+              />
             </div>
           </div>
         </div>
