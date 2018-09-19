77f99a211d1d900658e80ff130c6aa9af3fa14bc
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-09-19 00:15:43 -0400

    refactor components

--- front-end-libraries/pomodoro-clock/js/App.js
+++ front-end-libraries/pomodoro-clock/js/App.js
@@ -1,99 +1,31 @@
-class TimerLabel extends React.Component {
-  render() {
-    return (
-      <div id="timer-label">
-        <p>{this.props.label}</p>
-      </div>
-    );
-  }
-}
-
 class TimeLeft extends React.Component {
   render() {
     return (
-      <div id="timer-left">
-        <span id="tl-minutes">{this.props.minutesLeft}</span>:
-        <span id="tl-seconds">{this.props.secondsLeft}</span>
+      <div id="timer-label">
+        <span>{this.props.minutesLeft}</span>:
+        <span>{this.props.secondsLeft}</span>
       </div>
     );
   }
 }
 
-class BreakLabel extends React.Component {
+class Break extends React.Component {
   render() {
     return (
-      <div id="break-label">
-        Break Length
-        <div id="break-length">
-          <span id="bl-minutes">{this.props.breakMinutes}</span>:
-          <span id="bl-seconds">{this.props.breakSeconds}</span>
-        </div>
-        <BreakControls />
+      <div id="break-length">
+        <span>{this.props.breakMinutes}</span>:
+        <span>{this.props.breakSeconds}</span>
       </div>
     );
   }
 }
 
-class SessionLabel extends React.Component {
+class Session extends React.Component {
   render() {
     return (
-      <div id="session-label">
-        Session Length
-        <div id="session-length">
-          <span id="sl-minutes">{this.props.sessionMinutes}</span>:
-          <span id="sl-seconds">{this.props.sessionSeconds}</span>
-        </div>
-        <SessionControls />
-      </div>
-    );
-  }
-}
-
-class BreakControls extends React.Component {
-  render() {
-    return (
-      <div id="break-controls">
-        <div id="break-decrement">#break-decrement</div>
-        <div id="break-increment">#break-increment</div>
-      </div>
-    );
-  }
-}
-
-class SessionControls extends React.Component {
-  render() {
-    return (
-      <div id="session-controls">
-        <div id="session-decrement">#session-decrement</div>
-        <div id="session-increment">#session-increment</div>
-      </div>
-    );
-  }
-}
-
-class TimeSettings extends React.Component {
-  render() {
-    return (
-      <div id="timer-controls">
-        <BreakLabel
-          breakMinutes={this.props.breakMinutes}
-          breakSeconds={this.props.breakSeconds}
-        />
-        <SessionLabel
-          sessionMinutes={this.props.sessionMinutes}
-          sessionSeconds={this.props.sessionSeconds}
-        />
-      </div>
-    );
-  }
-}
-
-class TimeControls extends React.Component {
-  render() {
-    return (
-      <div id="controls">
-        <div id="start-stop">#start-stop</div>
-        <div id="reset">#reset</div>
+      <div id="session-length">
+        <span>{this.props.sessionMinutes}</span>:
+        <span>{this.props.sessionSeconds}</span>
       </div>
     );
   }
@@ -103,7 +35,6 @@ class Pomodoro extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      timerLabel: "Session",
       minutesLeft: "25",
       secondsLeft: "00",
       breakMinutes: "5",
@@ -117,18 +48,41 @@ class Pomodoro extends React.Component {
     return (
       <div className="text-center" id="pomodoro-clock">
         <h1>Tomato Clock</h1>
-        <TimerLabel label={this.state.timerLabel} />
         <TimeLeft
           minutesLeft={this.state.minutesLeft}
           secondsLeft={this.state.secondsLeft}
         />
-        <TimeSettings
-          breakMinutes={this.state.breakMinutes}
-          breakSeconds={this.state.breakSeconds}
-          sessionMinutes={this.state.sessionMinutes}
-          sessionSeconds={this.state.sessionSeconds}
-        />
-        <TimeControls />
+        <div id="timer-left">
+          <p>#timer-left div</p>
+        </div>
+        <div id="timer-controls">
+          <div id="break-label">
+            Break Length
+            <Break
+              breakMinutes={this.state.breakMinutes}
+              breakSeconds={this.state.breakSeconds}
+            />
+            <div id="break-controls">
+              <div id="break-decrement">#break-decrement</div>
+              <div id="break-increment">#break-increment</div>
+            </div>
+          </div>
+          <div id="session-label">
+            Session Length
+            <Session
+              sessionMinutes={this.state.sessionMinutes}
+              sessionSeconds={this.state.sessionSeconds}
+            />
+            <div id="session-controls">
+              <div id="session-decrement">#session-decrement</div>
+              <div id="session-increment">#session-increment</div>
+            </div>
+          </div>
+        </div>
+        <div id="controls">
+          <div id="start-stop">#start-stop</div>
+          <div id="reset">#reset</div>
+        </div>
       </div>
     );
   }
