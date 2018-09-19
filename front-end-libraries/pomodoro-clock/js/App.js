12eb65f197d7af2c61108bdb141d0ffab7798e6f
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-09-18 22:10:30 -0400

    refactor components

--- front-end-libraries/pomodoro-clock/js/App.js
+++ front-end-libraries/pomodoro-clock/js/App.js
@@ -1,36 +1,105 @@
-class Pomodoro extends React.Component {
+class TimerLabel extends React.Component {
   render() {
     return (
-      <div id="pomodoro-clock">
-        <h1>Pomodoro/Tomato Clock</h1>
-        <div id="timer-label">
-          <p>#timer-label div session/break</p>
-        </div>
-        <div id="timer-left">
-          <p>#timer-left div</p>
-        </div>
-        <div id="timing-controls">
-          <div id="break-label">
-            Break Length
-            <div id="break-length">#Break Length #</div>
-            <div id="break-controls">
-              <div id="break-decrement">#break-decrement</div>
-              <div id="break-increment">#break-increment</div>
-            </div>
-          </div>
-          <div id="session-label">
-            Session Length
-            <div id="session-length">#Break Length #</div>
-            <div id="session-controls">
-              <div id="session-decrement">#break-decrement</div>
-              <div id="session-increment">#break-increment</div>
-            </div>
-          </div>
-        </div>
-        <div id="controls">
-          <div id="start-stop">#start-stop</div>
-          <div id="reset">#reset</div>
-        </div>
+      <div id="timer-label">
+        <p>session/break</p>
+      </div>
+    );
+  }
+}
+
+class TimeLeft extends React.Component {
+  render() {
+    return (
+      <div id="timer-left">
+        <p>#timer-left div</p>
+      </div>
+    );
+  }
+}
+
+class BreakControls extends React.Component {
+  render() {
+    return (
+      <div id="break-controls">
+        <div id="break-decrement">#break-decrement</div>
+        <div id="break-increment">#break-increment</div>
+      </div>
+    );
+  }
+}
+
+class SessionControls extends React.Component {
+  render() {
+    return (
+      <div id="session-controls">
+        <div id="session-decrement">#break-decrement</div>
+        <div id="session-increment">#break-increment</div>
+      </div>
+    );
+  }
+}
+
+class BreakLabel extends React.Component {
+  render() {
+    return (
+      <div id="break-label">
+        Break Length
+        <div id="break-length">#Break Length</div>
+        <BreakControls />
+      </div>
+    );
+  }
+}
+
+class SessionLabel extends React.Component {
+  render() {
+    return (
+      <div id="session-label">
+        Session Length
+        <div id="session-length">#Session Length</div>
+        <SessionControls />
+      </div>
+    );
+  }
+}
+
+class TimeSettings extends React.Component {
+  render() {
+    return (
+      <div id="timer-controls">
+        <BreakLabel />
+        <SessionLabel />
+      </div>
+    );
+  }
+}
+
+class TimeControls extends React.Component {
+  render() {
+    return (
+      <div id="controls">
+        <div id="start-stop">#start-stop</div>
+        <div id="reset">#reset</div>
+      </div>
+    );
+  }
+}
+
+class Pomodoro extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="text-center" id="pomodoro-clock">
+        <h1>Tomato Clock</h1>
+        <TimerLabel />
+        <TimeLeft />
+        <TimeSettings />
+        <TimeControls />
       </div>
     );
   }
