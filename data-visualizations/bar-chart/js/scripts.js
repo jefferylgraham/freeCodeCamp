dfbe66193117afb8ee243ce7fffceb73b72db2af
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-09-28 19:52:22 -0400

    draw chart for GDPs

--- data-visualizations/bar-chart/js/scripts.js
+++ data-visualizations/bar-chart/js/scripts.js
@@ -1,1 +1,32 @@
+d3.json(
+  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+).then(function(data) {
+  var GDPs = [],
+    dates = [],
+    height = 400,
+    width = 600;
 
+  //Iterate through data object to get GDPs & Dates into array
+  for (var i = 0; i < data.data.length; i++) {
+    GDPs.push(data.data[i][1]);
+    dates.push(new Date(data.data[i][0]));
+  }
+
+  //Add chart of GDPs
+  var myChart = d3
+    .select("#visual")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .selectAll("rect")
+    .data(GDPs)
+    .enter()
+    .append("rect")
+    .attr("fill", "blue")
+    .attr("width", 1)
+    .attr("height", d => d)
+    .attr("x", (d, i) => i)
+    .attr("y", d => height - d);
+
+  console.log("here");
+});
