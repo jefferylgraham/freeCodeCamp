2e1888b22d5fde62c895bb0aa6d7e722728cbb87
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-10-04 23:18:22 -0400

    draw map

--- data-visualizations/chloropleth-map/js/scripts.js
+++ data-visualizations/chloropleth-map/js/scripts.js
@@ -1,4 +1,35 @@
-d3.json("https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json")
-.then(function(data) {
-    console.log(data);
-})
+var topologyUrl =
+  "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json";
+
+d3.json(topologyUrl).then(function(us) {
+  var width = 960,
+    height = 700,
+    path = d3.geoPath();
+
+  var choropleth = d3
+    .select("#chloropleth")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width);
+
+  choropleth
+    .append("g")
+    .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter()
+    .append("path")
+    .attr("d", path);
+
+  choropleth
+    .append("path")
+    .attr("class", "county-borders")
+    .attr(
+      "d",
+      path(
+        topojson.mesh(us, us.objects.counties, function(a, b) {
+          return a !== b;
+        })
+      )
+    );
+});
