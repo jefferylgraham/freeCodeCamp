0686c734954d91849c0bc0db41aea5f94fd469bc
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-10-04 22:44:05 -0400

    add starter files for chloropleth project

---
+++ data-visualizations/chloropleth-map/js/scripts.js
@@ -0,0 +1,4 @@
+d3.json("https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json")
+.then(function(data) {
+    console.log(data);
+})
