f32d954f428377bc3de532227ecde1f9d4ab80ed
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-10-05 23:03:57 -0400

    refactor code to add Promises to load both json files async

--- data-visualizations/chloropleth-map/js/scripts.js
+++ data-visualizations/chloropleth-map/js/scripts.js
@@ -1,17 +1,28 @@
-var topologyUrl =
+var topologyJson =
   "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json";
 
-d3.json(topologyUrl).then(function(us) {
+var educationJson =
+  "https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json";
+
+//save json data in array for Promises
+var data = [topologyJson, educationJson];
+
+Promise.all(data.map(url => d3.json(url))).then(function(values) {
+  //topology json
+  var us = values[0];
+
   var width = 960,
     height = 700,
     path = d3.geoPath();
 
+  //define svg area
   var choropleth = d3
     .select("#chloropleth")
     .append("svg")
     .attr("height", height)
     .attr("width", width);
 
+  //define group for drawing topology
   choropleth
     .append("g")
     .attr("class", "counties")
@@ -21,6 +32,7 @@ d3.json(topologyUrl).then(function(us) {
     .append("path")
     .attr("d", path);
 
+  //add borders to counties
   choropleth
     .append("path")
     .attr("class", "county-borders")
