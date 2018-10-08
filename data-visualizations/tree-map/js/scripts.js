76083186da19ddd9115d76b95f065f6e7b2c2f14
Jeffery L. Graham <jeffery.lemar.graham@gmail.com>
2018-10-07 21:12:44 -0400

    add data attributes to tiles

--- data-visualizations/tree-map/js/scripts.js
+++ data-visualizations/tree-map/js/scripts.js
@@ -50,6 +50,10 @@ Promise.all(dataFiles.map(url => d3.json(url))).then(function(values) {
 
   nodes
     .append("rect")
+    .attr("class", "tile")
+    .attr("data-name", d => d.data.name)
+    .attr("data-category", d => d.data.category)
+    .attr("data-value", d => d.data.value)
     .attr("width", d => d.x1 - d.x0)
     .attr("height", d => d.y1 - d.y0)
     .attr("fill", d => color(d.data.category));
